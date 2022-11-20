7dd6a35b22e1dae26ff5fcde0ac3b32e7eb91295
raghav3575 <raghav3575.be20@chitkara.edu.in>
2022-11-21 00:54:02 +0530

    first commit

---
+++ src/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/userRegistration" , {
+
+}).then(() => {
+    console.log(`connection successful`)
+}).catch((e) => {
+    console.log(`connection failed`)
+})
+
+
